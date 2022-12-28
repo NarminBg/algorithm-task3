9d48f6b43ce376db19b8728e1729592320bc8fe0
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-28 19:02:43 +0400

    Add project files.

---
+++ algorithm task3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace algorithm_task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int new_number = 4000000 + 10 * number + 2;
+            Console.WriteLine(new_number);
+        }
+    }
+}
