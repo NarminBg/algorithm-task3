5d961f99d26eafda5ed7659350806e2e5145e836
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-29 16:06:54 +0400

    change

--- algorithm task3/Program.cs
+++ algorithm task3/Program.cs
@@ -6,8 +6,20 @@
         {
             Console.WriteLine("Eded:");
             int number = Convert.ToInt32(Console.ReadLine());
-            int new_number = 4000000 + 10 * number + 2;
-            Console.WriteLine(new_number);
+            int count = (int)Math.Log10(number)+1;
+           
+
+            if (count==5)
+            { 
+                int new_number = (400000 + number) * 10 + 2;
+                Console.WriteLine(new_number);
+            }
+            
+            else
+            {
+                Console.WriteLine("Eded 5 reqemli olmalidir");
+            }
+            
         }
     }
 }
